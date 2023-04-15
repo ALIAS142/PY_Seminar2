217068634cb78edfbaa9dc75f6147828f816bb52
Anna <kochurovaav@gmail.com>
2023-04-15 16:10:42 +0300

    Changes are made

--- PY_Seminar2/Task01.py
+++ PY_Seminar2/Task01.py
@@ -1,31 +1,14 @@
-# Дано натуральное число A > 1. Определите, каким по счету числом Фибоначчи оно является, то есть выведите такое число n,
-# что φ(n)=A. Если А не является числом Фибоначчи, выведите число -1.
+# Дано натуральное число A > 1. Определите, каким по счету числом Фибоначчи оно является,
+# то есть выведите такое число n, что φ(n)=A. Если А не является числом Фибоначчи, выведите число -1.
 
-# a = input("Enter number:   ")
-# a = int(a)
-#
-# a1 = 0
-# a2 = 1
-# fib = a1 + a2
-# if a == 0:
-#     print(0)
-# elif a == fib:
-#      print(fib)
-# else:
-#     print(-1)
-
-a = int(input())
-if a == 0:
-    print(0)
+n1=0
+n2=1
+k=1
+a=int(input())
+while n2<a:
+    n1,n2=n2,n2+n1
+    k+=1
+if n2!=a:
+    print("-1")
 else:
-    fib_prev, fib_next = 0, 1
-n = 1
-while (fib_next <= a):
-    if fib_next == a:
-
-print(n)
-    break
-    fib_prev, fib_next = fib_next, fib_prev + fib_next
-    n += 1
-else:
-    print(-1)
+    print(k)
